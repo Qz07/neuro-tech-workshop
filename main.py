c95f4bdd7f2bfd6b771cee9d10653a31643fa6b9
unknown <sarazhengqz@gmail.com>
2023-11-16 19:11:33 -0800

    try 2

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys 
-
-def print_sys_props():
-    ...
-def print_hello_world():
-    print('Hello world!')
-    
 def main():
-    
     print('Hello World!')
-
+print('hahahaha')
+print('hhah')
 main()
